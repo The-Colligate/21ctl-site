45f996cc5f5defed1e73ad5b5bd9bc1955435b1b
Pedro Ogheneochuko H <pedroogheneochuko@gmail.com>
2022-12-28 00:36:55 +0100

    finish up light page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      defaultTheme="system"
+      attribute="class"
+      enableSystem
+      enableColorScheme
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -8,9 +9,26 @@ module.exports = {
     extend: {
       colors: {
         "primary-orange": "#F64E1F",
+        "primary-orange-light": "rgba(246, 78, 31, 0.2);",
       },
       fontFamily: {
         lato: ["Lato", "sans-serif"],
+      },
+      screens: {
+        largeTablet: { max: "1024px" },
+        // => @media (max-width: 1024px) { ... }
+
+        tablet: { max: "780px" },
+        // => @media (max-width: 780px) { ... }
+
+        smallTablet: { max: "600px" },
+        // => @media (max-width: 600px) { ... }
+
+        phone: { max: "500px" },
+        // => @media (max-width: 500px) { ... }
+
+        smallPhone: { max: "375px" },
+        // => @media (max-width: 350px) { ... }
       },
     },
   },
