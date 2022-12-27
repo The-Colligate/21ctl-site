48cb572e93f442a58e026f60badb089ee314d6be
Pedro Ogheneochuko H <pedroogheneochuko@gmail.com>
2022-12-27 02:25:15 +0100

    initial commit

---
+++ icons/arrow-right.js
@@ -0,0 +1,29 @@
+export function ArrowRight(props) {
+  return (
+    <svg
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clip-path="url(#clip0_678_700)">
+        <path
+          d="M6.72385 13.3113L19.2043 13.3113L15.3211 9.42811L16.4996 8.2496L22.3922 14.1422L16.4996 20.0347L15.3211 18.8562L19.2043 14.973L6.72385 14.973L6.72385 13.3113Z"
+          fill="currentColor"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_678_700">
+          <rect
+            width="20"
+            height="20"
+            fill="white"
+            transform="translate(14.1426) rotate(45)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
