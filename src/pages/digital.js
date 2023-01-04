4f915be4d46aa8299278d2ca18d72686ee363076
“Akpughe” <davidakpughe2@gmail.com>
2023-01-04 02:21:59 +0100

    page update

---
+++ src/pages/digital.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Digital from '../pages/light/digital';
+const DigitalInfra = () => {
+  return (
+    <div>
+      <Digital />
+    </div>
+  );
+};
+
+export default DigitalInfra;
