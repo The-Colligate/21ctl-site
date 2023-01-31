b2323bec1ac4bb990b1db6d79b8fd0dd2f84150f
blessedmadukoma <blessedmadukoma@gmail.com>
2023-01-31 18:15:49 +0100

    pushing live

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca7185d778e96f72"; }
+/******/ 		__webpack_require__.h = function() { return "1982cad95d53fdb1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
