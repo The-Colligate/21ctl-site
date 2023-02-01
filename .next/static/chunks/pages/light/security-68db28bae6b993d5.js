2a0609db0159fa80c52645b67fc43dbf20ce426b
David Ajaba <davidajabas32014@gmail.com>
2023-02-01 12:57:13 +0100

    Pushing latest build as pulled from main

--- .next/static/chunks/pages/light/security-68db28bae6b993d5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[772],{2050:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/light/security",function(){return u(185)}])}},function(n){n.O(0,[976,290,32,185,774,888,179],function(){return n(n.s=2050)}),_N_E=n.O()}]);
