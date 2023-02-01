f038d517a8429f81d13b0678682c6728a98f19a2
“Akpughe” <davidakpughe2@gmail.com>
2023-02-01 10:24:21 +0100

    adjust responsiveness

--- src/pages/light/power.js
+++ src/pages/light/power.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 import { LightNavbar, Footer, SocialIcons } from '@components/layout/';
 import { motion } from 'framer-motion';
-import { leftTransitionVarient, rightTansitionVarient } from '@components/constants/animations';
+import {
+  leftTransitionVarient,
+  rightTansitionVarient,
+} from '@components/constants/animations';
 
 // import power from '/public/logo/Power.png';
 
@@ -19,31 +22,34 @@ const Power = () => {
           <div className="w-full min-h-[50%] bg_gradient_r"></div>
         </div>
         <div className="w-full min-h-screen flex flex-col z-[1]">
-        <LightNavbar  />
+          <LightNavbar />
 
           <section className="sm:px-28 px-8 my-16 sm:py-0 pt-16">
             <div className="flex sm:flex-row flex-col sm:space-x-16 space-x-0 items-center">
-            <motion.div
-              variants={leftTransitionVarient}
-              animate="visible"
-              initial="hidden"
-               className="max-w-lg">
+              <motion.div
+                variants={leftTransitionVarient}
+                animate="visible"
+                initial="hidden"
+                className="max-w-lg"
+              >
                 <h1 className="text-6xl tracking-normal capitalize font-bold whitespace-pre-wrap largeTablet:text-5xl tablet:text-3xl smallTablet:text-2xl">
-                  
-                    creating <span className="text-primary-orange leading-8">energy</span>{' '}
-              
-                 for the<span className="text-primary-orange leading-8"> future</span>{' '}
-               
+                  creating{' '}
+                  <span className="text-primary-orange leading-8">energy</span>{' '}
+                  for the
+                  <span className="text-primary-orange leading-8"> future</span>{' '}
                 </h1>
                 <p className="text-2xl mt-6 largeTablet:text-xl smallTablet:text-lg">
-                  We are investing in sustainable clean energy in Africa to power the future across industries and communities creating possibilities.
+                  We are investing in sustainable clean energy in Africa to
+                  power the future across industries and communities creating
+                  possibilities.
                 </p>
               </motion.div>
               <motion.div
-              variants={rightTansitionVarient}
-              animate="visible"
-              initial="hidden"
-               className="">
+                variants={rightTansitionVarient}
+                animate="visible"
+                initial="hidden"
+                className=""
+              >
                 <img
                   src="https://res.cloudinary.com/dqsggbqmf/image/upload/v1675058119/21ctl/logo/Power_m6awqs.png"
                   width={800}
@@ -54,52 +60,53 @@ const Power = () => {
             </div>
           </section>
 
-        <section className='energy'>
-          <section className="sm:mb-0 mb-16 py-20 mx-20 tablet:mx-0">
-            <div className="flex items-center space-x-10 space-y-0 my-10 mx-10 mb-0 tablet:flex-col largeTablet:flex-col largeTablet:text-center largeTablet:space-y-10 largeTablet:space-x-0">
-              <div className="flex-1">
-                <h1 className="sm:text-5xl text-3xl font-semibold tracking-medium mb-8">
-                  <span className="text-primary-orange">
-                    Sustainable Solutions
-                  </span>
-                </h1>
-                <p className="leading-8 text-xl">
-                  Elevate your energy with our sustainable solutions that
-                  provide clean, reliable power while reducing your carbon
-                  footprint and energy costs.
-                </p>
+          <section className="energy">
+            <section className="sm:mb-0 mb-16 py-20 mx-20 tablet:mx-0">
+              <div className="flex items-center space-x-10 space-y-0 my-10 mx-10 mb-0 tablet:flex-col largeTablet:flex-col largeTablet:text-center largeTablet:space-y-10 largeTablet:space-x-0">
+                <div className="flex-1">
+                  <h1 className="sm:text-5xl text-3xl font-semibold tracking-medium mb-8">
+                    <span className="text-primary-orange">
+                      Sustainable Solutions
+                    </span>
+                  </h1>
+                  <p className="leading-8 sm:text-xl text-lg">
+                    Elevate your energy with our sustainable solutions that
+                    provide clean, reliable power while reducing your carbon
+                    footprint and energy costs.
+                  </p>
+                </div>
+                <div className="flex-1">
+                  <img
+                    src="https://res.cloudinary.com/dqsggbqmf/image/upload/v1673290646/image_1_rmjwqv.webp"
+                    className="w-full"
+                  />
+                </div>
               </div>
-              <div className="flex-1">
-                <img
-                  src="https://res.cloudinary.com/dqsggbqmf/image/upload/v1673290646/image_1_rmjwqv.webp"
-                  className="w-full"
-                />
-              </div>
-            </div>
-          </section>
+            </section>
 
-
-          <section className="sm:mb-0 mb-16 py-20   mx-20 tablet:mx-0">
-            <div className="flex items-center space-x-10 space-y-0 tablet:flex-col-reverse">
-              <div className="flex-1 tablet:mt-10 tablet:px-10">
-                <img
-                  src="https://res.cloudinary.com/dqsggbqmf/image/upload/v1673291336/Group_1_g9wnmq.webp"
-                  className="w-full"
-                />
+            <section className="sm:mb-0 mb-16 py-20   mx-20 tablet:mx-0">
+              <div className="flex items-center space-x-10 space-y-0 tablet:flex-col-reverse">
+                <div className="flex-1 tablet:mt-10 tablet:px-10">
+                  <img
+                    src="https://res.cloudinary.com/dqsggbqmf/image/upload/v1673291336/Group_1_g9wnmq.webp"
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex-1">
+                  <h1 className="sm:text-5xl text-3xl font-semibold tracking-medium mb-8">
+                    <span className="text-primary-orange">
+                      Grid Connections
+                    </span>{' '}
+                    & Substations
+                  </h1>
+                  <p className="leading-8 sm:text-xl text-lg max-w-md sm:px-0 px-5">
+                    Our advanced grid connections and substations ensure a
+                    stable, reliable power supply, with our expert team and
+                    state-of-the-art technology.
+                  </p>
+                </div>
               </div>
-              <div className="flex-1">
-                <h1 className="sm:text-5xl text-3xl font-semibold tracking-medium mb-8">
-                  <span className="text-primary-orange">Grid Connections</span>{' '}
-                  & Substations
-                </h1>
-                <p className="leading-8 text-xl max-w-md">
-                  Our advanced grid connections and substations ensure a stable,
-                  reliable power supply, with our expert team and
-                  state-of-the-art technology.
-                </p>
-              </div>
-            </div>
-          </section>
+            </section>
           </section>
           <section className="sm:mb-0 mb-16 py-20  mx-20 tablet:mx-10">
             <div className="flex items-center space-x-10 space-y-0 my-10  mb-0 tablet:flex-col largeTablet:flex-col largeTablet:text-center largeTablet:space-y-10 largeTablet:space-x-0">
@@ -108,7 +115,7 @@ const Power = () => {
                   <span className="text-primary-orange">Brighten</span> the
                   Future <span className="text-primary-orange">Solar</span>{' '}
                 </h1>
-                <p className="leading-8 text-xl">
+                <p className="leading-8 sm:text-xl text-lg">
                   Our solar panels harness the power of the sun to generate
                   clean, renewable energy for your home or business. Save money
                   on energy costs and reduce your carbon footprint with our
