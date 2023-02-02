2ac9f3c9889d6b8044b7bc771dac331c26b88cab
“Akpughe” <davidakpughe2@gmail.com>
2023-02-02 01:30:51 +0100

    fix: wrong comment syntax

--- src/pages/light/security.js
+++ src/pages/light/security.js
@@ -32,10 +32,10 @@ const Security = () => {
         <meta name="description" content="" />
       </Head>
       <div className="max-w-full min-h-screen flex flex-col overflow-x-hidden relative bg-white dark:bg-[#252525] opacity-90">
-        // <div className="absolute w-full h-full flex justify-between items-center">
-        //   <div className="w-full min-h-[50%] bg_gradient_l"></div>
-        //   <div className="w-full min-h-[50%] bg_gradient_r"></div>
-        // </div>
+         {/* <div className="absolute w-full h-full flex justify-between items-center">
+           <div className="w-full min-h-[50%] bg_gradient_l"></div>
+           <div className="w-full min-h-[50%] bg_gradient_r"></div>
+         </div> */}
         <div className="w-full min-h-screen flex flex-col z-[1]">
           <LightNavbar />
 
