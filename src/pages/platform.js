27602b0a08b06eab517bf34a45606db6ebe3e25e
“Akpughe” <davidakpughe2@gmail.com>
2023-02-02 16:33:06 +0100

    remove: gradient bf\g

--- src/pages/platform.js
+++ src/pages/platform.js
@@ -12,8 +12,8 @@ const Platform = () => {
       </Head>
       <div className="max-w-full min-h-screen flex flex-col overflow-x-hidden relative">
         <div className="absolute w-full h-full flex justify-between items-center">
-          <div className="w-full min-h-[50%] bg_gradient_l"></div>
-          <div className="w-full min-h-[50%] bg_gradient_r"></div>
+          <div className="w-full min-h-[50%] "></div>
+          <div className="w-full min-h-[50%] "></div>
         </div>
         <div className="w-full min-h-screen flex flex-col z-[1]">
           <LightNavbar />
