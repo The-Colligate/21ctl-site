3eb428cc65ced0d1235736460a500267b359bdc1
David Ajaba <davidajabas32014@gmail.com>
2023-02-10 12:38:38 +0100

    Built the new changes made by Davak, Caleb and Blessed

--- .next/static/chunks/pages/light/academy-20317b9350942ba2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[478],{17549:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/light/academy",function(){return u(93673)}])}},function(n){n.O(0,[983,253,365,756,673,774,888,179],function(){return n(n.s=17549)}),_N_E=n.O()}]);
--- .next/static/chunks/pages/light/security-28eee4291f7b62dc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[772],{46373:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/light/security",function(){return u(3402)}])}},function(n){n.O(0,[983,59,756,402,774,888,179],function(){return n(n.s=46373)}),_N_E=n.O()}]);
