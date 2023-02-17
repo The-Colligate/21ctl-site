035560544df1cdecf1e44b75b6ec2bd918040a87
blessedmadukoma <blessedmadukoma@gmail.com>
2023-02-17 10:00:45 +0100

    fixed mobile nnavbar

---
+++ .next/static/chunks/pages/light/academy-c958c65d99f9e58f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[478],{20426:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/light/academy",function(){return u(72446)}])}},function(n){n.O(0,[914,869,85,378,446,774,888,179],function(){return n(n.s=20426)}),_N_E=n.O()}]);
